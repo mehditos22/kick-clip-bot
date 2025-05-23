9830d1cf0a5c0af74a8dbd789514a05c532c1fa7
mehditos22 <elmehdiaitboubker0@gmail.com>
2025-05-23 21:50:54 +0100

    Fix: Ajout des headers HTTP pour éviter erreur 403

--- detectNewClips.js
+++ detectNewClips.js
@@ -10,7 +10,11 @@ async function fetchClips() {
   try {
     const res = await axios.get(URL, {
       headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Referer': `https://kick.com/${STREAMER}`,
+        'Connection': 'keep-alive'
       }
     });
     const $ = load(res.data);
